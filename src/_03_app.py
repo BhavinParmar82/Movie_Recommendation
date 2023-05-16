f70278cbc2fd396cacac454ce35241e008c66504
BhavinParmar82 <bhavin9922@gmail.com>
2023-05-16 09:56:49 +0530

    background image added

--- src/_03_app.py
+++ src/_03_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['POST', 'GET'])
 def submit():
-    movie_list = None
+    movie_list = []
     if request.method == 'POST':
         name = request.form['name']
         movie_list = recommend(name)
